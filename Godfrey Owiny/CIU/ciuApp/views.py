ccaa154224a01bfa59fbcbd9821868e82fced225
Goddie-Owiny <goddiegodfreyz07@gmail.com>
2024-05-17 17:51:31 +0300

    Godfrey Owiny

---
+++ Godfrey Owiny/CIU/ciuApp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .forms import *
+from django.contrib import messages
+
+# Create your views here.
+
+def homePage(request):
+    return render(request, 'ciuApp/home.html')
+    
+
+def register(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Student Registered Successfully')
+            return redirect('/')  
+        else:
+            messages.error(request, 'Student Registration Failed')
+    else:
+        form = StudentForm()
+
+    return render(request, 'ciuApp/register.html', {'form': form})
+
+
